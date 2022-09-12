782df36649510ebf9245b8647618897d493bf0e8
Peter Wi <peter.h.wi96@gmail.com>
2022-09-12 13:57:20 -0400

    add caculating offset

--- myapp.py
+++ myapp.py
@@ -1,59 +1,68 @@
 from flask import Flask, jsonify
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import requests
 
 app = Flask(__name__)
-myheader = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhSTUIiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBycHJvIHJudXQgcnNsZSByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkyMjk2MDE4LCJpYXQiOjE2NjA3NjAwMTh9.Ud4qSIXGglbXaYeK-JDzL9GolEskKk9aCGrl79NMDY4'}
+# myheader = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhSTUIiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBycHJvIHJudXQgcnNsZSByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkyMjk2MDE4LCJpYXQiOjE2NjA3NjAwMTh9.Ud4qSIXGglbXaYeK-JDzL9GolEskKk9aCGrl79NMDY4'}
+# myheader = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhSRFQiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBycHJvIHJudXQgcnNsZSByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkzNDg4MDQxLCJpYXQiOjE2NjE5NTIwNDF9.uk4UyLwyQeLjnoE6jxKPNCxfkzs0mFTq_09cfuyV74U'}
+# myheader = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhSNkIiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBybnV0IHJwcm8gcnNsZSByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkyMjk1NDQ0LCJpYXQiOjE2NjA3NTk0NDR9.bILcGIrPRXPWRrWBZDKRLsZdtTKKqPUpZ4NZZ-U3k5g'}
+# myheader = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhRNVIiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBycHJvIHJudXQgcnNsZSByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkyMzIxOTk2LCJpYXQiOjE2NjA3ODU5OTZ9.Rw2SpXEMA3YVx1-O1W0ZamKq2BwRnUpOw_fQCMRn0z8'}
+myheader = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhRUEYiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBycHJvIHJudXQgcnNsZSByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkyMzIyMTc4LCJpYXQiOjE2NjA3ODYxNzh9.t4-tjP-pBKe-wdbYLTL9t-h7wAOWsAlu-cGurSkfJiU'}
+# myheader = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhRTkQiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBybnV0IHJwcm8gcnNsZSByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkyMzIyMzQ0LCJpYXQiOjE2NjA3ODYzNDR9.-kAqRq3x5D5J0nCgzOm-2ATMbz9e7EZYXUiitEt6h4k'}
 
 
 @app.route("/heartrate/last", methods=["GET"])
 def get_heart_rate():
     # Return the most recent heartrate.
-    timeoffset = 15
     dayinput = date.today()
-    myurl = "https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/{}min/time/00:00/23:59.json".format(
-        timeoffset)
+    myurl = "https://api.fitbit.com/1/user/-/activities/heart/date/{}/1d.json".format(
+        dayinput)
     resp = requests.get(myurl, headers=myheader).json()
     activity_dataset = resp["activities-heart-intraday"]["dataset"]
     while (len(activity_dataset) == 0):
         # if there is no data on that day, check the previous days.
         dayinput -= timedelta(days=1)
-        myurl = "https://api.fitbit.com/1/user/-/activities/heart/date/{}/1d/{}min/time/00:00/23:59.json".format(
-            dayinput, timeoffset)
+        myurl = "https://api.fitbit.com/1/user/-/activities/heart/date/{}/1d.json".format(
+            dayinput)
         resp = requests.get(myurl, headers=myheader).json()
         activity_dataset = resp["activities-heart-intraday"]["dataset"]
     recent = activity_dataset[-1]
+    # retrieve heartrate, timeoffset (min)
     heartrate = recent["value"]
-    ret = {"heart-rate": heartrate, "time offset": timeoffset}
+    timeoffset = datetime.now() - datetime.combine(dayinput, datetime.strptime(
+        recent["time"], "%H:%M:%S").time())
+    ret = {"heart-rate": heartrate, "time offset": timeoffset.seconds//60}
     return jsonify(ret)
 
 
 @app.route("/steps/last", methods=["GET"])
 def get_steps():
     # Return the most recent step.
-    timeoffset = 15
     dayinput = date.today()
-    myurl = "https://api.fitbit.com/1/user/-/activities/steps/date/today/1d/{}min.json".format(
-        timeoffset)
+    myurl = "https://api.fitbit.com/1/user/-/activities/steps/date/today/1d.json"
     resp = requests.get(myurl, headers=myheader).json()
     activity_step = resp["activities-steps"]
-    myurl = "https://api.fitbit.com/1/user/-/activities/distance/date/today/1d/{}min.json".format(
-        timeoffset)
+    timeoffset = resp["activities-steps-intraday"]["datasetInterval"]
+    myurl = "https://api.fitbit.com/1/user/-/activities/distance/date/today/1d.json"
     resp = requests.get(myurl, headers=myheader).json()
     activity_distance = resp["activities-distance"]
+    activity_dataset = resp["activities-distance-intraday"]["dataset"]
     while (len(activity_step) == 0 or len(activity_distance) == 0):
         # if ther eis no data on that day, check the previous days.
         dayinput -= timedelta(days=1)
-        myurl = "https://api.fitbit.com/1/user/-/activities/steps/date/{}/1d/{}min.json".format(
-            dayinput, timeoffset)
+        myurl = "https://api.fitbit.com/1/user/-/activities/steps/date/{}/1d.json".format(
+            dayinput)
         resp = requests.get(myurl, headers=myheader).json()
         activity_step = resp["activities-steps"]
-        myurl = "https://api.fitbit.com/1/user/-/activities/distance/date/{}/1d/{}min.json".format(
-            dayinput, timeoffset)
+        myurl = "https://api.fitbit.com/1/user/-/activities/distance/date/{}/1d.json".format(
+            dayinput)
         resp = requests.get(myurl, headers=myheader).json()
         activity_distance = resp["activities-distance"]
+        activity_dataset = resp["activities-distance-intraday"]["dataset"]
+    recent = activity_dataset[-1]
+    timeoffset = datetime.now() - datetime.combine(dayinput, datetime.min.time())
     ret = {"step-count": activity_step[-1]["value"],
-           "distance": activity_distance[-1]["value"], "time offset": timeoffset}
+           "distance": activity_distance[-1]["value"], "time offset": timeoffset.seconds//60}
     return jsonify(ret)
 
 
